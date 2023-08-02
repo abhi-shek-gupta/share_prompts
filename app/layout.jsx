297ced9a63a18ba148efe3f4190d89866170326f
abhigput_gartner <abhishekc.gupta@gartner.com>
2023-08-02 19:44:19 +0530

    Added Navigation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
-import Nav from "@components/Nav";
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
